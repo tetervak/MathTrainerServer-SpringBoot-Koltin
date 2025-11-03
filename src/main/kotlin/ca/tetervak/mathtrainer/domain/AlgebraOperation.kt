06d8b9eb27b6a7c50494f5efd4f31e7a76bea0e0
Alex Tetervak <tetervak@sheridancollege.ca>
2025-11-03 16:44:03 -0500

    matched the newer Android code

--- src/main/kotlin/ca/tetervak/mathtrainer/domain/AlgebraOperation.kt
+++ src/main/kotlin/ca/tetervak/mathtrainer/domain/AlgebraOperation.kt
@@ -18,4 +18,15 @@ enum class AlgebraOperation {
             MULTIPLICATION -> a * b
             DIVISION -> a / b
         }
+
+    companion object {
+        fun fromSymbol(symbol: Char): AlgebraOperation =
+            when(symbol) {
+                '+' -> ADDITION
+                '-' -> SUBTRACTION
+                'x' -> MULTIPLICATION
+                '/' -> DIVISION
+                else -> throw IllegalArgumentException("Invalid operation symbol: $symbol")
+            }
+    }
 }
